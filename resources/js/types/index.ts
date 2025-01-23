976bda594de040a550da41c0aa33fdd8c95e54c0
EmilienKopp <emilien.kopp@gmail.com>
2025-01-24 01:00:15 +0900

    feat: enhance user role management and form handling in inertia
    - Updated `superUseForm` to accept optional object parameters for improved flexibility.
    - Introduced `getUserRoleName` and `getAllUserRoles` functions to retrieve user roles from the page props, enhancing user role management.
    - Modified `PageProps` interface to include a `flash` property for toast notifications, improving feedback mechanisms.
    - Added new `types/index.ts` file to define user and page properties, enhancing type safety and clarity in the application.

---
+++ resources/js/types/index.ts
@@ -0,0 +1,15 @@
+import type { User as UserModel } from '$models';
+
+export type User = UserModel;
+
+export type PageProps<
+  T extends Record<string, unknown> = Record<string, unknown>,
+> = T & {
+  auth: {
+    user: User;
+  },
+  enums: {
+    roles: SelectOption[];
+
+  }
+};
