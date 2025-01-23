0b3332730d62701e732cb94966af5b4c24389f6b
EmilienKopp <emilien.kopp@gmail.com>
2025-01-24 00:09:38 +0900

    feat: introduce DataTable type definitions and refactor Wrapper component for improved type safety and structure

--- resources/js/Components/Display/DataTable/Wrapper.svelte
+++ resources/js/Components/Display/DataTable/Wrapper.svelte
@@ -1,27 +1,13 @@
 <script lang="ts">
   import { run } from 'svelte/legacy';
 
-  import { query } from '$lib/stores';
   import { exists } from '$lib/utils/assessing';
-  import type { TableProps } from '$types/components/Table';
+  import { query } from '$lib/stores/global/query.svelte';
   import { twMerge } from 'tailwind-merge';
   import TableActions from './Action.svelte';
   import TableCell from './Cell.svelte';
   import TableHeader from './Header.svelte';
   import Pagination from './Pagination.svelte';
-
-  interface Props {
-    data?: TableProps<any>['data'] | TableProps<any>['paginatedData'];
-    paginated?: TableProps<any>['paginated'];
-    paginatedData?: TableProps<any>['paginatedData'];
-    headers?: TableProps<any>['headers'];
-    onRowClick?: TableProps<any>['onRowClick'];
-    onDelete?: TableProps<any>['onDelete'];
-    model?: TableProps<any>['model'];
-    className?: string;
-    actions?: TableProps<any>['actions'];
-    searchStrings?: TableProps<any>['searchStrings'];
-  }
 
   let {
     data = undefined,
@@ -33,19 +19,19 @@
     model = 'user',
     className = '',
     actions = undefined,
-    searchStrings = $bindable([])
+    searchStrings = $bindable([]),
   }: Props = $props();
 
   let tableData: any[] = $state(data);
 
-  if (!searchStrings?.length && $query.param('search')) {
-    searchStrings = [$query.param('search')?.toString() || ''];
+  if (!searchStrings?.length && query.param('search')) {
+    searchStrings = [query.param('search')?.toString() || ''];
   }
 
-  let pageIndex = $derived($query.param('page') || 1);
+  let pageIndex = $derived(query.param('page') || 1);
 
   run(() => {
-    if(paginated) {
+    if (paginated) {
       tableData = data?.data ?? paginatedData?.data ?? [];
     }
     if (paginatedData && data?.length) {
@@ -55,39 +41,6 @@
 
   let hasActions = $derived(Boolean(onDelete || actions?.length));
 </script>
-
-{#if tableData?.length === 0}
-  <div class="text-center text-gray-500 p-4">No data available</div>
-{:else}
-  <div class="overflow-x-auto shadow-md rounded-lg">
-    <table class="table table-zebra table-sm w-full {className}">
-      <thead>
-        <TableHeader {headers} {hasActions} />
-      </thead>
-
-      <tbody>
-        {#each tableData ?? [] as row}
-          <tr class={twMerge(
-            'hover:bg-base-300 transition-colors duration-200',
-            onRowClick && 'cursor-pointer'
-          )}>
-            {#each headers as header}
-              <TableCell {row} {header} {searchStrings} {onRowClick} />
-            {/each}
-            
-            {#if actions?.length}
-              <TableActions {actions} {row} />
-            {/if}
-          </tr>
-        {/each}
-      </tbody>
-    </table>
-  </div>
-
-  {#if paginated || paginatedData?.length || exists(data?.data)}
-    <Pagination paginatedData={data} pageIndex={Number(pageIndex)} />
-  {/if}
-{/if}
 
 <style>
   table {
@@ -103,3 +56,38 @@
     z-index: 10;
   }
 </style>
+
+{#if tableData?.length === 0}
+  <div class="text-center text-gray-500 p-4">No data available</div>
+{:else}
+  <div class="overflow-x-auto shadow-md rounded-lg">
+    <table class="table table-zebra table-sm w-full {className}">
+      <thead>
+        <TableHeader {headers} {hasActions} />
+      </thead>
+
+      <tbody>
+        {#each tableData ?? [] as row}
+          <tr
+            class={twMerge(
+              'hover:bg-base-300 transition-colors duration-200',
+              onRowClick && 'cursor-pointer'
+            )}
+          >
+            {#each headers as header}
+              <TableCell {row} {header} {searchStrings} {onRowClick} />
+            {/each}
+
+            {#if actions?.length}
+              <TableActions {actions} {row} />
+            {/if}
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  </div>
+
+  {#if paginated || paginatedData?.length || exists(data?.data)}
+    <Pagination paginatedData={data} pageIndex={Number(pageIndex)} />
+  {/if}
+{/if}
