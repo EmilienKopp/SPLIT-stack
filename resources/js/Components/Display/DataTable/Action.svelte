690b7fa371eab47f9be990b0bd54f9e2d782603e
EmilienKopp <emilien.kopp@gmail.com>
2025-01-24 00:13:36 +0900

    refactor: enhance Action component in DataTable for improved action handling and type safety

--- resources/js/Components/Display/DataTable/Action.svelte
+++ resources/js/Components/Display/DataTable/Action.svelte
@@ -1,31 +1,49 @@
 <script lang="ts">
-  import type { TableAction } from '$types/components/Table';
+  import type { DataAction } from '$types/common/dataDisplay';
+  import { Link } from '@inertiajs/svelte';
   import { twMerge } from 'tailwind-merge';
 
   interface Props {
-    actions: TableAction<any>[];
+    actions: DataAction<any>[];
     row: any;
   }
 
   let { actions, row }: Props = $props();
 </script>
 
-<td class="text-center flex gap-2">
+{#snippet buttonContent(action: DataAction<any>, row: any)}
+  {#if action.icon}
+    {@const SvelteComponent = action.icon(row)}
+    <SvelteComponent class="w-4 h-4" />
+  {/if}
+  {action.label}
+{/snippet}
+
+<td class="text-center flex justify-center gap-2">
   {#each actions as action}
     {#if !action.hidden?.(row)}
-      <button
-        onclick={() => action.callback(row)}
-        class={twMerge(
-          'mx-1 px-1 hover:underline flex items-center gap-1s',
-          action.css?.(row)
-        )}
-      >
-        {#if action.icon}
-          {@const SvelteComponent = action.icon(row)}
-          <SvelteComponent class="w-4 h-4" />
-        {/if}
-        {action.label}
-      </button>
+      {#if action?.callback}
+        <button
+          type="button"
+          onclick={() => action.callback?.(row)}
+          class={twMerge(
+            'mx-1 px-1 hover:underline flex items-center gap-1s',
+            action.css?.(row)
+          )}
+        >
+          {@render buttonContent(action, row)}
+        </button>
+      {:else if action?.href}
+        <Link
+          href={action.href(row)}
+          class={twMerge(
+            'mx-1 px-1 hover:underline flex items-center gap-1',
+            action.css?.(row)
+          )}
+        >
+          {@render buttonContent(action, row)}
+        </Link>
+      {/if}
     {/if}
   {/each}
 </td>
