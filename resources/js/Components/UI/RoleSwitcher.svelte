434ccda6509c2cd9a9f0812768fa5a97c1cf6ad2
EmilienKopp <emilien.kopp@gmail.com>
2025-01-24 00:11:37 +0900

    feat: add RoleSwitcher component for user role selection
    - Introduced a new RoleSwitcher component to allow users to select their roles.
    - Integrated Select component for role selection with options derived from available user roles.
    - Utilized RoleContext to manage the selected and available roles, enhancing user experience and context management.

---
+++ resources/js/Components/UI/RoleSwitcher.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Select from '$components/DataInput/Select.svelte';
+  import { asSelectOptions } from '$lib/utils/formatting';
+  import { getAllUserRoles, getUserRoleName } from '$lib/inertia';
+  import { RoleContext } from '$lib/stores/global/roleContext.svelte';
+
+  RoleContext.selected = getUserRoleName();
+  RoleContext.available = getAllUserRoles();
+</script>
+
+<div class="form-control w-full max-w-xs">
+  <Select
+    name="role"
+    bind:value={RoleContext.selected}
+    options={asSelectOptions(RoleContext.available)}
+    class="select-primary select w-full bg-white shadow-sm transition-all hover:shadow-md"
+  />
+</div>
