05717041b8309adb6d0fd19fb770aef1637967f0
EmilienKopp <emilien.kopp@gmail.com>
2025-02-13 09:56:42 +0900

    feat: add show and addMetadata methods to ProjectController

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,16 +4,34 @@ namespace App\Http\Controllers;
 
 use Inertia\Inertia;
 use App\Repositories\ProjectRepository;
-use Illuminate\Support\Facades\Log;
+use App\Models\Project;
+use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = ProjectRepository::cached('index');
+        $projects = ProjectRepository::cache('index');
 
         return Inertia::render('Project/Index', [
             'projects' => $projects,
         ]);
+    }
+
+    public function show($id)
+    {
+        $project = ProjectRepository::find($id);
+
+        return Inertia::render('Project/Show', [
+            'project' => $project,
+        ]);
+    }
+
+    public function addMedata(Project $project, Request $request)
+    {
+        $project->setMetadata($request->all());
+        $project->save();
+
+        return back();
     }
 }
